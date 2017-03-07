88450b42c6028fcd3730173ef71ddfab21d26a5b
yangf6 <yangf6@uw.edu>
2017-03-07 13:13:06 -0800

    d

---
+++ Untitled.R
@@ -0,0 +1,4 @@
+library(dplyr);
+library(plotly);
+library(ggplot2);
+library(plyr)
--- dataSummary.R
+++ dataSummary.R
@@ -1,3 +1,13 @@
 library(ggplot2)
 library(plotly)
 library(plyr)
+
+
+#u.s umeployment
+rawUnemploy <- read.table("./data/currency/Brazilian Real.csv")
+
+df.timeStamp
+df.timeStamp[0] <- "hi"
+df.unemploy <- data.frame(rawUnemploy["Jan"])
+
+
